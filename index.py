f8fdd18ed11950da41576344b9c824a7f97809d8
Muhammed Owais <muhammedowaismalkani@gmail.com>
2026-06-29 17:36:07 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+def Mysum(num1,num2,num3):
+    return (num2 - num1 * num3) 
+
+y = Mysum(12, 1, 1)
+
+for x in range (0, 1):
+    print (y)
+
+
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the first number"))
+sum = num1 + num2 
+print (sum)
