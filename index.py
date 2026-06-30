36d04819240a7f9d317d98f6f1ef9f0690626916
Muhammed Owais <muhammedowaismalkani@gmail.com>
2026-06-30 16:39:30 +0500

    Refactor Mysum and add myfunction as assignment
    Updated Mysum function and added myfunction as assignment. Commented out user input section for clarity.

--- index.py
+++ index.py
@@ -7,7 +7,32 @@ for x in range (0, 1):
     print (y)
 
 
-num1 = int(input("Enter the first number"))
-num2 = int(input("Enter the first number"))
-sum = num1 + num2 
-print (sum)
+# num1 = int(input("Enter the first number"))
+# num2 = int(input("Enter the first number"))
+# sum = num1 + num2 
+# print (sum)
+
+
+
+def myfunction(fullname):
+    return "Muhammed Owais"
+
+num1 = int(input("Enter a first number"))
+num2 = int(input("Enter a second number"))
+result = num1 * num2 
+print ("The result is", result)
+    
+
+print(myfunction(fullname=str))
+
+
+
+
+
+    
+
+
+
+
+
+   
