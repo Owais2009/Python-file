bc02117e4db408cd4b6a3a69838c42dd5d9c0f7f
Muhammed Owais <muhammedowaismalkani@gmail.com>
2026-07-03 15:22:40 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+# --Assignment---
+
+  
+# Task 1: 
+Studentlist = ["Owais","Hassan","ALi","Bilal"]
+OtherStudentlist = ["Ahmed","Zain","Ayan"]
+Studentlist.extend(OtherStudentlist) 
+print(Studentlist)
+
+# Task 2:
+Studentlist = ["Owais","Hassan","ALi","Bilal"]
+Studentlist[2:2] = ["Ahmed","Zain"]
+print(Studentlist)
+
+
+# Task 3:
+Studentlist = ["Owais","Hassan","ALi","Bilal"]
+Studentlist.pop(0)
+Studentlist.pop(-1)
+print(Studentlist)     
